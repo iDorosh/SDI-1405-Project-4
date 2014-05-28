0aba3e1def8d9e2d0258a9d947425f9a46a50eac
Ian Dorosh <iDorosh@Ians-MacBook-Pro.local>
2014-05-27 21:30:54 -0500

    Number Function
    Created the first function in my code which is converting a number with
    decimal places and rounding it to the 2 nearest decimal places.

--- Dorosh_Ian_assignment4.js
+++ Dorosh_Ian_assignment4.js
@@ -1,1 +1,21 @@
-alert("JavaScript works!");
+// Ian Dorosh
+// SDI 1405
+// Project 4
+
+//Functions
+
+//Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
+// Number function
+
+var roundingNum = function(number) {
+	var finalnumber;
+	
+	finalnumber = number.toFixed(2);
+	
+	return (number + " is rounded 2 decimal places to " + finalnumber);
+	
+}
+
+
+console.log (roundingNum(45.45644))
+	
