658edf3d57b0acf55002a50b94c4bea15ec447b9
Ian Dorosh <iDorosh@Ians-MacBook-Pro.local>
2014-05-30 16:22:07 -0500

    Created Array Function
    Created an array function for finding the smallest number that is still
    greater than a given number. Also created a global variable for the
    array of given numbers.

--- Dorosh_Ian_assignment4.js
+++ Dorosh_Ian_assignment4.js
@@ -2,6 +2,7 @@
 // SDI 1405
 // Project 4
 
+var numberSet = [5,3,2,6,7,8,23,554,344,2,1,]
 //Functions
 
 //Format a number to use a specific number of decimal places, as for money: 2.1 → 2.10
@@ -17,5 +18,38 @@ var roundingNum = function(number) {
 }
 
 
+//Find the smallest value in an array that is greater than a given number
+//Array Function
+
+var lowestNumber = function(numberSet, givenNumber) {
+		
+		var bigNumbers = [];
+		var smallestNumber;
+	
+		for(i=0; i < numberSet.length; i++) {
+			
+			if (nuberSet[i] > givenNumber) {
+					bigNumbers.push(numberSet[i]);
+			};
+		};
+		
+		smallestNumber = bigNumbers[0];
+		
+		for (j = 0; j < bigNumbers.length; j++) {
+			if (smallestNumber > bigNumbers[j]) {
+				smallestNumber = bigNumbers[j];
+			};
+		};
+		
+			return(smallestNumber + " is the smallest number that is larger than " + givenNumber + ".")
+};
+
+
+
+
+
+
+//Main Code
 console.log (roundingNum(3.249))
+consol.log (lowestNumber(numberSet, 4))
 	
